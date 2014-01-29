f2ace8cd8c3c044b8c732240b548578f431cc570
Brian Samson <brian@briansamson.com>
2014-01-29 00:35:56 -0600

    embarassing typo

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -2,7 +2,7 @@ class NetworkController < ApplicationController
   def status
     @netinfo = {}
     [:bitcoin, :litecoin, :dogecoin].each do |network|
-      client = Bitcion::Client.local(network)
+      client = Bitcoin::Client.local(network)
       @netinfo[network] = client.getinfo rescue {"errors" => "Unable to connect to daemon"}
     end
   end
