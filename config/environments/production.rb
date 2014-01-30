4be10fa8f459e6c33ffdd5b5d93f6cbaf614b098
Brian Samson <brian@briansamson.com>
2014-01-29 22:24:19 -0600

    add css file to assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Btcbrite::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( hackathon.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
